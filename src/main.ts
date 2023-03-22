5ba51d553e8e6b86ee45bf851531930d6d035205
lexenson <alexander.assmann91@gmail.com>
2023-03-22 17:30:04 +0100

    Add bounding walls and fix mouse

--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,83 @@
 import {
   Engine,
-  Render,
-  Runner,
-  MouseConstraint,
-  Mouse,
   Composite,
   Bodies,
-  Detector,
+  Body,
+  Mouse,
+  MouseConstraint,
+  Runner,
 } from 'matter-js';
+import kaboom, { Comp, PosComp, RotateComp } from 'kaboom';
 
-const engine = Engine.create({
-  gravity: {
-    scale: 0,
-  },
-});
-const world = engine.world;
+const engine = Engine.create({ gravity: { scale: 0 } });
 
-const render = Render.create({
-  element: document.body,
-  engine,
-  options: {
-    width: 800,
-    height: 600,
-    showAngleIndicator: true,
-  },
+const k = kaboom({
+  global: false,
+  width: 600,
+  height: 600,
+  background: [230, 210, 210],
 });
 
-Render.run(render);
+const mouse = Mouse.create(k.canvas);
+const mouseConstraint = MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    stiffness: 0.2,
+  },
+});
 
-const runner = Runner.create();
-Runner.run(runner, engine);
+Composite.add(engine.world, mouseConstraint);
 
-const item = Bodies.rectangle(100, 100, 50, 50);
-Composite.add(world, item);
-
-const bucket = Bodies.circle(300, 300, 100, { isStatic: true, isSensor: true });
-Composite.add(world, bucket);
-
-Composite.add(world, [
-  Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-  Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-  Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
+k.add([
+  k.pos(40, 40),
+  k.rect(50, 50),
+  k.anchor('center'),
+  k.color(170, 100, 100),
+  matterRect(),
+  k.rotate(0),
 ]);
 
-const mouse = Mouse.create(render.canvas),
-  mouseConstraint = MouseConstraint.create(engine, {
-    mouse: mouse,
-    constraint: {
-      stiffness: 0.2,
-      render: {
-        visible: false,
-      },
+Runner.run(engine);
+
+type MatterRectComp = {
+  body?: Body;
+} & PosComp &
+  RotateComp;
+
+function matterRect(): Comp {
+  return {
+    require: ['pos', 'rotate'],
+    add(this: MatterRectComp) {
+      const { x, y } = this.pos;
+      this.body = Bodies.rectangle(x, y, 50, 50);
+
+      Composite.add(engine.world, this.body);
     },
-  });
+    update(this: MatterRectComp) {
+      if (!this.body) return;
 
-Composite.add(world, mouseConstraint);
+      this.pos.x = this.body.position.x;
+      this.pos.y = this.body.position.y;
+      this.angle = (this.body.angle * 180) / Math.PI;
+    },
+    destroy(this: MatterRectComp) {
+      if (!this.body) return;
 
-render.mouse = mouse;
-
-Render.lookAt(render, {
-  min: { x: 0, y: 0 },
-  max: { x: 800, y: 600 },
-});
-
-const detector = Detector.create({});
-Detector.setBodies(detector, [item, bucket]);
-
-function gameLoop() {
-  const collisions = Detector.collisions(detector);
-  console.log(collisions);
-  // if (Collision.collides(item, bucket, Pairs.create({}))) {
-  // console.log('collision');
-  // Composite.remove(world, item);
-  // }
-  requestAnimationFrame(gameLoop);
+      Composite.remove(engine.world, this.body);
+    },
+  };
 }
 
-gameLoop();
+Composite.add(engine.world, [
+  Bodies.rectangle(300, -20, 600, 40, { isStatic: true }),
+  Bodies.rectangle(300, 600 + 20, 600, 40, { isStatic: true }),
+  Bodies.rectangle(-20, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(600 + 20, 300, 40, 600, { isStatic: true }),
+]);
+
+Mouse.setScale(mouse, {
+  x: 0.5,
+  y: 0.5,
+});
+
+Mouse.setOffset(mouse, { x: 0, y: 0 });
