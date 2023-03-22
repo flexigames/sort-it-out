589e60db57e0f6ec6013ad2616b96bc55f2d236e
Finn Pauls <mbp@pauls.dev>
2023-03-23 00:07:25 +0100

    Add minion helper

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import {
   Mouse,
   MouseConstraint,
   Runner,
-} from 'matter-js';
-import kaboom, { Comp, PosComp, RectComp, RotateComp, TextComp } from 'kaboom';
-import { sample } from 'lodash';
+} from "matter-js";
+import kaboom, { Comp, PosComp, RectComp, RotateComp, TextComp } from "kaboom";
+import { sample } from "lodash";
 
 const engine = Engine.create({ gravity: { scale: 0 } });
 
@@ -20,8 +20,8 @@ const k = kaboom({
 });
 
 const colorMap = {
-  red: k.Color.fromHex('#EB1F26'),
-  blue: k.Color.fromHex('#05A8DC'),
+  red: k.Color.fromHex("#EB1F26"),
+  blue: k.Color.fromHex("#05A8DC"),
 };
 
 const mouse = Mouse.create(k.canvas);
@@ -35,9 +35,9 @@ const mouseConstraint = MouseConstraint.create(engine, {
 Composite.add(engine.world, mouseConstraint);
 
 const score = k.add([
-  k.text('0'),
+  k.text("0"),
   k.pos(k.width() / 2, 40),
-  k.anchor('center'),
+  k.anchor("center"),
   k.color(0, 0, 0),
   {
     score: 0,
@@ -51,17 +51,18 @@ function spawnItem() {
   const startVelocity = 0.005;
   const colorId = sample(Object.keys(colorMap)) as ColorId;
   const item = k.add([
-    'item',
+    "item",
+    { colorId },
     k.pos(40, k.height() / 2),
     k.rect(32, 32),
-    k.anchor('center'),
+    k.anchor("center"),
     k.color(colorMap[colorId]),
     matterRect(startVelocity),
     k.area(),
     k.rotate(0),
   ]);
 
-  item.onCollide('bucket-' + colorId, () => {
+  item.onCollide("bucket-" + colorId, () => {
     item.destroy();
     score.score++;
   });
@@ -75,8 +76,10 @@ function spawn() {
 
 spawn();
 
-addBucket('red', { x: k.width() - 100, y: k.height() / 2 - 150 });
-addBucket('blue', { x: k.width() - 100, y: k.height() / 2 + 150 });
+addBucket("red", { x: k.width() - 100, y: k.height() / 2 - 150 });
+addBucket("blue", { x: k.width() - 100, y: k.height() / 2 + 150 });
+
+addMinion();
 
 type Position = {
   x: number;
@@ -87,14 +90,73 @@ type ColorId = keyof typeof colorMap;
 
 function addBucket(colorId: ColorId, position: Position) {
   k.add([
-    'bucket',
-    'bucket-' + colorId,
+    "bucket",
+    "bucket-" + colorId,
     colorId,
     k.pos(position.x, position.y),
     k.rect(100, 100),
-    k.anchor('center'),
+    k.anchor("center"),
     k.color(colorMap[colorId]),
     k.area(),
+  ]);
+}
+
+function addMinion() {
+  const position = { x: 40, y: k.height() / 2 };
+  const speed = 1;
+  const minion = k.add([
+    "minion",
+    k.pos(position.x, position.y),
+    k.circle(12),
+    k.anchor("center"),
+    k.color(0, 0, 0),
+    k.area(),
+    k.rotate(0),
+    {
+      update: () => {
+        const closestItem = k.get("item").sort((a, b) => {
+          const aDist = Math.sqrt(
+            Math.pow(a.pos.x - minion.pos.x, 2) +
+              Math.pow(a.pos.y - minion.pos.y, 2)
+          );
+          const bDist = Math.sqrt(
+            Math.pow(b.pos.x - minion.pos.x, 2) +
+              Math.pow(b.pos.y - minion.pos.y, 2)
+          );
+          return aDist - bDist;
+        })[0];
+
+        if (!closestItem) return;
+
+        const distance = Math.sqrt(
+          Math.pow(closestItem.pos.x - minion.pos.x, 2) +
+            Math.pow(closestItem.pos.y - minion.pos.y, 2)
+        );
+
+        const colorId = closestItem.colorId;
+        const targetBucket = k.get("bucket-" + colorId)[0];
+
+        if (distance < 5) {
+          const angleToBucket =
+            Math.atan2(
+              targetBucket.pos.y - minion.pos.y,
+              targetBucket.pos.x - minion.pos.x
+            ) + k.rand(-0.1, 0.1);
+
+          closestItem.body.force = {
+            x: Math.cos(angleToBucket) * 0.01,
+            y: Math.sin(angleToBucket) * 0.01,
+          };
+        }
+
+        const angle = Math.atan2(
+          closestItem.pos.y - minion.pos.y,
+          closestItem.pos.x - minion.pos.x
+        );
+        minion.pos.x += Math.cos(angle) * speed;
+        minion.pos.y += Math.sin(angle) * speed;
+      },
+    },
   ]);
 }
 
@@ -112,7 +174,7 @@ interface MatterRectComp extends Comp {
 
 function matterRect(startVelocity: number): MatterRectComp {
   return {
-    require: ['pos', 'rotate', 'rect'],
+    require: ["pos", "rotate", "rect"],
     add(this: MatterRectContext) {
       const { x, y } = this.pos;
       this.body = Bodies.rectangle(x, y, this.width, this.height);
@@ -149,12 +211,12 @@ Mouse.setScale(mouse, {
 
 Mouse.setOffset(mouse, { x: 0, y: 0 });
 
-document.querySelector('#button-spawn-rate')?.addEventListener('click', () => {
+document.querySelector("#button-spawn-rate")?.addEventListener("click", () => {
   if (score.score >= 5) {
-    console.log('Upgrade bought');
+    console.log("Upgrade bought");
     score.score -= 5;
     timeBetweenSpawnsInSeconds *= 0.7;
   } else {
-    alert('Not enough cash :(');
+    alert("Not enough cash :(");
   }
 });
